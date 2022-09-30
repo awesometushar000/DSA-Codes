19bda46529a0042724976bf1bd4db62a2dc8a8bf
Nandini saagar <96643952+Nandinisaagar@users.noreply.github.com>
2022-09-30 21:56:11 +0530

    Update BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int binarySearch(int array[], int x, int low, int high) {
+  
   while (low <= high) {
     int mid = low + (high - low) / 2;
 
@@ -19,12 +21,11 @@ int binarySearch(int array[], int x, int low, int high) {
 
 int main(void) {
   int array[] = {3, 4, 5, 6, 7, 8, 9};
-  int n = sizeof(array) / sizeof(array[0]);
   int x = 4;
+  int n = sizeof(array) / sizeof(array[0]);
   int result = binarySearch(array, x, 0, n - 1);
   if (result == -1)
     printf("Not found");
   else
     printf("Element is found at index %d", result);
-  return 0;
 }
